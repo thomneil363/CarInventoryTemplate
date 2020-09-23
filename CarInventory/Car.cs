2ed903bfeca24c2c01ebefd46a4d5165f4fd20eb
Thomas Neilson <thomneil363@outlook.com>
2020-09-23 14:24:40 -0400

    Finished Code

---
+++ CarInventory/Car.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CarInventory
+{
+    class Car
+    {
+        public int year;
+        public string make;
+        public string colour;
+        public int mileage;
+
+        public Car(int _year, string _make, string _colour, int _mileage)
+        {
+            year = _year;
+            make = _make;
+            colour = _colour;
+            mileage = _mileage;
+        }
+    }
+
+}
--- CarInventory/Form1.cs
+++ CarInventory/Form1.cs
@@ -11,6 +11,7 @@ namespace CarInventory
 {
     public partial class Form1 : Form
     {
+        List<Car> cars = new List<Car>();
 
         public Form1()
         {
@@ -19,6 +20,24 @@ namespace CarInventory
 
         private void addButton_Click(object sender, EventArgs e)
         {
+            int year = Convert.ToInt32(yearInput.Text);
+            string make = makeInput.Text;
+            string colour = colourInput.Text;
+            int mileage = Convert.ToInt32(mileageInput.Text);
+
+            Car newCar = new Car(year, make, colour, mileage);
+
+            cars.Add(newCar);
+
+            outputLabel.Text = "";
+
+            foreach (Car c in cars)
+            {
+                outputLabel.Text += c.year + "\n";
+                outputLabel.Text += c.make + "\n";
+                outputLabel.Text += c.colour + "\n";
+                outputLabel.Text += c.mileage + "\n" + "\n";
+            }
 
         }
     }
